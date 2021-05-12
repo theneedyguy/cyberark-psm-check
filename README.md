64b3848962d80cdba2b6b6cba5f1b67f63d88768
admin <admin@0x01.host>
2021-05-12 14:47:03 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Cyberark PSM Service Check
+
+This simple service checks the status of the Cyberark PSM service and exposes an HTTP endpoint where it reports the state of the PSM service. It serves as a healthcheck endpoint for load balancers.
+
+If the PSM service is running the PSM server will return **PASS** on port 80 if the PSM service is down it will return **FAIL**.
+
+This is a single executable and does not require IIS unlike Cyberark's implementation which does use IIS to do essentially the same. Another advantage is that the executable is installed in a few seconds and uses a tiny amount of resources to run.
+
+## Installation
+
+*(Optional) Have a look at the source code or even build the executable yourself using Golang on a Windows host instead of downloading the installer.*
+
+Download the svccheck.msi and install it on the PSM servers. You can check if it works by navigating to http://localhost on the PSM server. 
---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/kardianos/service"
+	"golang.org/x/sys/windows"
+	"golang.org/x/sys/windows/svc/mgr"
+)
+
+var logger service.Logger
+
+type program struct{}
+
+const (
+	// Host name of the HTTP Server
+	host = "0.0.0.0"
+	// Port of the HTTP Server
+	port = "80"
+	// Cyber Ark PSM Name
+	windowsService = "Cyber-Ark Privileged Session Manager"
+)
+
+func (p *program) Start(s service.Service) error {
+	// Start should not block. Do the actual work async.
+	go p.run()
+	return nil
+}
+func (p *program) run() {
+	// Do work here
+	http.HandleFunc("/", reportState)
+	err := http.ListenAndServe(host+":"+port, nil)
+	if err != nil {
+		log.Fatal("Error Starting the HTTP Server : ", err)
+		return
+	}
+}
+func (p *program) Stop(s service.Service) error {
+	// Stop should not block. Return with a few seconds.
+	return nil
+}
+
+func reportState(w http.ResponseWriter, r *http.Request) {
+	manager, err := mgr.Connect()
+	if err != nil {
+		log.Println(err)
+		fmt.Fprintf(w, "FAIL")
+	}
+	serviceInstance, err := manager.OpenService(windowsService)
+	if err != nil {
+		log.Println(err)
+		fmt.Fprintf(w, "FAIL")
+	} else {
+		serviceState, _ := serviceInstance.Query()
+		switch serviceState.State {
+		case windows.SERVICE_RUNNING:
+			fmt.Fprintf(w, "PASS")
+		default:
+			fmt.Fprintf(w, "FAIL")
+		}
+	}
+}
+
+func main() {
+	svcConfig := &service.Config{
+		Name:        "PSMSVCCheck",
+		DisplayName: "CyberArk PSM Service Check",
+		Description: "CyberArk PSM Service Check",
+	}
+
+	prg := &program{}
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	logger, err = s.Logger(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = s.Run()
+	if err != nil {
+		logger.Error(err)
+	}
+}
